af916e4740066922d90e06043a5aa1aeec20e0cd
Guy Wilson <Guywilsonjr@gmail.com>
2022-03-07 18:17:04 -0800

    better figures for a histogram better figures for a histogra
    Took 42 minutes

--- visual/main.py
+++ visual/main.py
@@ -1,25 +1,53 @@
 import pandas as pd
-from dash import dcc, Dash, html
+import plotly as plotly
+from dash import dcc, Dash, html, Input, Output
 import plotly.express as px
 
 
 df = pd.read_csv('playerInfo2018.csv')
 app = Dash(__name__)
+
+
 fig = px.histogram(df['field_goals'])
-app.layout = html.Div(children=[
 
-    html.Div(children='''
-        Main Window.
-    '''),
+dropdown_label_key: str = 'label'
+dropdown_value_key: str = 'value'
 
-    dcc.Graph(
-        figure=fig
-    )
-])
+class DCCGraph:
+    app: Dash
+    fig: plotly.graph_objs.Figure
+    df: pd.DataFrame
 
-fig.show()
+    def __init__(self, app, fig, df):
+        self.app = app
+        self.fig = fig
+        self.df = df
+        self.rev = {i: col for i, col in enumerate(df.columns)}
+        app.layout = html.Div(children=[
+            html.Div(children='''
+                Main Window.
+            '''),
+            dcc.Graph(id='hist',
+                figure=fig
+
+            ),
+            dcc.Dropdown(
+                [{dropdown_label_key: col, dropdown_value_key: i} for i, col in enumerate(df.columns)],
+                id='slider'
+            )
+        ])
+        #fig.show()
+        @app.callback(
+            Output(component_id='hist', component_property='figure'),
+            Input(component_id='slider', component_property='value')
+        )
+        def update_output_div(input_value):
+            print(f'Got: {input_value}')
+            col = self.rev[input_value]
+            print(f'Using: {col}')
+            return px.histogram(df[col])
+
 
 if __name__ == '__main__':
-    exit()
-    app.run_server(debug=True)
+    g.app.run_server(debug=True)
 
