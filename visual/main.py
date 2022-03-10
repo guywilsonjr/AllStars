fac27825c3753307c56930d342d0343335f836a7
Guy Wilson <Guywilsonjr@gmail.com>
2022-03-09 21:03:41 -0800

    Add More documentation and cleanup Add More documentation and cleanu
    Took 37 seconds

--- visual/main.py
+++ visual/main.py
@@ -1,6 +1,5 @@
 import functools
 import logging
-import os
 import sys
 from typing import Dict, List
 
@@ -23,12 +22,28 @@ def setup_logging():
 
 logger = setup_logging()
 df: pd.DataFrame = pd.read_csv('playerInfo2018.csv')
+
+
 dropdown_label_key: str = 'label'
 dropdown_value_key: str = 'value'
-default_view: str = 'points'
+default_feature: str = 'points'
 
 
-class DCCGraph:
+class MainApplication:
+    """
+    This is the main application. There are multiple pieces that are needed to draw the plots.
+    The plotly library is based on html.
+
+    App
+    - Data(in dataframe form)
+    - AppLayout
+    - - Graph
+    - - - Dropdown
+    - - - Figure
+
+    When this class is created with the given dataframe and default column the plots are initialized, but not yet displayed
+    They become displayed when someone calls this objects `app.run_server` function
+    """
     app: Dash
     df: pd.DataFrame
     dropdown_component: dcc.Dropdown
@@ -36,47 +51,24 @@ class DCCGraph:
     dropdown_id: str = 'dropdown'
     hist_id: str = 'hist'
 
-    def __init__(
-        self,
-        df: pd.DataFrame,
-        default_col: str
-    ) -> None:
-        self.rev = {i: col for i, col in enumerate(df.columns)}
+    def initialize(self, df: pd.DataFrame, default_col: str):
         self.df = df
         self.default_col = default_col
         self.setup_dropdown()
         self.setup_layout()
 
+        # This is the trigger function that updates the graph on dropdown change
         @functools.lru_cache
-        @self.app.callback(
-            Output(component_id=self.hist_id, component_property='figure'),
-            Input(component_id=self.dropdown_id, component_property='value')
-        )
-        def update_dropdown(dropdown_selection: str) -> plotly.graph_objs.Figure:
+        @self.app.callback(Output(component_id='hist', component_property='figure'), Input(component_id='dropdown', component_property='value'))
+        def trigger_update_on_dropdown_change(dropdown_selection: str) -> plotly.graph_objs.Figure:
             ic(f'Updating output after receiving dropdown value: {dropdown_selection}')
             return self.create_histogram(col=dropdown_selection)
-
-    def create_histogram(self, col: str, percentile: int=66):
-        # TODO: create variable size partitions
-        column_data: pandas.Series = self.df[col]
-        hist_title = f'{col} Distribution'.replace('_', ' ').title()
-
-        half_quantile = column_data.quantile(.5)
-        first_half = df[df[col] < half_quantile][col]
-        second_half = df[df[col] >= half_quantile][col]
-
-        fig = go.Figure()
-        fig.add_trace(go.Histogram(x=first_half))
-        fig.add_trace(go.Histogram(x=second_half))
-
-        fig.update_layout(barmode='stack')
-        return fig
 
     def setup_layout(self) -> None:
         default_fig = px.histogram(df[self.default_col])
         self.app = Dash(__name__)
         self.app.layout = html.Div(children=[
-            html.Div(children='''Main Window.'''),
+            html.Div(children='''Rising Stars'''),
             dcc.Graph(
                 id='hist',
                 figure=default_fig
@@ -93,8 +85,28 @@ class DCCGraph:
             persistence=True
         )
 
+    def create_histogram(self, col: str, percentile: int = 66):
+        # TODO: create variable size partitions
+        column_data: pandas.Series = self.df[col]
+        hist_title = f'{col} Distribution'.replace('_', ' ').title()
+
+        half_quantile = column_data.quantile(.5)
+        first_half = df[df[col] < half_quantile][col]
+        second_half = df[df[col] >= half_quantile][col]
+
+        fig = go.Figure()
+        fig.add_trace(go.Histogram(x=first_half))
+        fig.add_trace(go.Histogram(x=second_half))
+
+        fig.update_layout(barmode='stack')
+        return fig
+
+    # Constructor
+    def __init__(self, df: pd.DataFrame, default_col: str) -> None:
+        self.initialize(df, default_col)
+
 
 if __name__ == '__main__':
-    graph = DCCGraph(df, default_view)
+    graph = MainApplication(df, default_feature)
     graph.app.run_server(debug=True, port=9999)
 
