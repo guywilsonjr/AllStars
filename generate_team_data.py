71b71a7eab3ec0c2d31a681b89494ebf787330a3
Guy Wilson <Guywilsonjr@gmail.com>
2022-04-20 16:52:16 -0700

    Rachit is totally wrong

--- generate_team_data.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-import pandas as pd
-from sportsipy.ncaab.teams import Teams
-
-
-df_list: List[pd.DataFrame] = [team.dataframe for team in Teams()]
-pd.concat(df_list).to_csv('allteams.csv')
